c313cd5971b53790d4f4b389f19b6a07a953af25
MrFive <13070156190@163.com>
2018-01-29 19:51:11 +0800

    增加状态维度，动作维度

--- 2 Missile_fight/Missile.py
+++ 2 Missile_fight/Missile.py
@@ -8,13 +8,28 @@ class MissileAI:
     def __init__(self):
         near,mid,long,moon,blood = 6,4,3,1,0
         self.init_state = np.array([near,mid,long,moon,blood]*2) # 双方仓库导弹数目、卫星个数，血量
+        self.state = self.init_state
+        self.state_dim = len(self.state)  #状态的维度是10
+        self.action_dim = 2  # 动作的维度是2
+        self.first = True   # 是否是先手
+        self.hit = np.array([[[0.9, 0.7], [0.75, 0.5], [0, 0], [0, 0], [0, 0]],
+                             [[0.8, 0.8], [0.7, 0.7], [0.7, 0.6], [0.7, 0], [0.7, 60]],
+                             [[0.7, 0.9], [0.65, 0.8], [0.6, 0.75], [0, 0], [0.55, 100]]])
+        # hit[i,j]第i个导弹命中j个地方的概率[命中率，损毁率]
 
-    def step(self):
-        pass
+
+    def step(self,action):
+
+
+
+        if sum(self.state[:3]) + sum(self.state[5:8]) == 0:
+            done = True
+        else:
+            done = False
 
     def reset(self):
-        pass
-
+        self.state = self.init_state
+        return self.state
 
     def render(self):
         pass
