b8d3523fceb0ecafb9f01bc43e3993e5a0ed8298
MrFive <13070156190@163.com>
2018-03-06 11:03:00 +0800

    增加A3C算法，但是有一些错误，因为输出网络系数太大

---
+++ 6 TSP/main_a3c.py
@@ -0,0 +1,18 @@
+from TSP_Burma14 import ENV
+import numpy as np
+import A3C_Dis as A3C
+import matplotlib.pyplot as plt
+
+env = ENV()
+para = A3C.Para(env,
+                units_a=10,
+                units_c=10,
+                MAX_GLOBAL_EP=10000,
+                UPDATE_GLOBAL_ITER=14,
+                gamma=0.9,
+                ENTROPY_BETA=0.01,
+                LR_A=0.001,
+                LR_C=0.001)
+RL = A3C.A3C(para)
+RL.run()
+# RL.display()
